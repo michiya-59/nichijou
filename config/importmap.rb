fbfd14038129e9eece7787e9b7606e6e4e62f2f5
michiya-59 <nishino.michiya0509@gmail.com>
2023-10-16 22:36:00 +0900

    jQueryとJSを個別に使えるように修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "controllers/index"
