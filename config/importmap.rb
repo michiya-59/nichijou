475661a084cb563acbd8d8cb17fac75c4ce34fae
michiya-59 <nishino.michiya0509@gmail.com>
2023-11-04 14:55:39 +0900

    バージョン変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,7 @@ pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/inde
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.1/app/assets/javascripts/rails-ujs.esm.js"
 # 正しいBootstrapのバージョンのみを保持
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.1/dist/js/bootstrap.esm.js"
 pin_all_from "app/javascript/shared", under: "shared"
 pin "marked", to: "https://ga.jspm.io/npm:marked@9.1.2/lib/marked.esm.js"
 # slickーslider
