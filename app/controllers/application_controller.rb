c5e19a6f5c1e2afd3fb76850e4fa4e38d8bc26a2
michiya-59 <nishino.michiya0509@gmail.com>
2023-11-07 01:05:21 +0900

    カスタムエラー画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   include(AdminSessionsHelper)
   around_action :catch_exception
 
-  def catch_exception
-    yield
-  rescue StandardError => e
-    LineNotifier.notify(e)
-    raise e # 例外を再度raiseして、アプリケーションの標準のエラー処理を行う
-  end
-
   # ログインされていない場合またはURLが直接操作されてた場合の処理
   def authenticate_user
     return if logged_in? && valid_referrer?
@@ -32,5 +25,40 @@ class ApplicationController < ActionController::Base
   # URLが直接操作されているかどうかの確認
   def valid_referrer?
     request.referer.present? && request.referer.include?(request.base_url)
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html{render "errors/not_found", status: :not_found}
+      format.json{render json: {error: "Not found"}, status: :not_found}
+      format.any{head :not_found}
+    end
+  end
+
+  private
+
+  def catch_exception
+    yield
+  rescue ActiveRecord::RecordNotFound, ActionController::RoutingError => e
+    render_404(e)
+  rescue StandardError => e
+    notify_and_render_exception(e)
+  end
+
+  def render_500 exception
+    LineNotifier.notify(exception)
+    logger.error "Internal Server Error: #{exception.message}"
+    render "errors/internal_server_error", status: :internal_server_error
+  end
+
+  # 通知してから例外を再発生させるメソッド
+  def notify_and_render_exception exception
+    LineNotifier.notify(exception)
+    case exception
+    when ActiveRecord::RecordNotFound, ActionController::RoutingError
+      render_404(exception)
+    else
+      render_500(exception)
+    end
   end
 end
