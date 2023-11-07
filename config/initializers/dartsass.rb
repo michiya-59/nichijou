a200f8ca58ce4e0e91eb92234e46219fde3cb17d
michiya-59 <nishino.michiya0509@gmail.com>
2023-11-07 23:46:09 +0900

    微修正

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -3,6 +3,5 @@
 Rails.application.config.dartsass.builds = {
   "admin_index.scss" => "admin_index.css",
   "index.scss" => "index.css",
-  "destyle.scss" => "destyle.css",
-  "error.scss" => "error.css"
+  "destyle.scss" => "destyle.css"
 }
